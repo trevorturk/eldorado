4f7aec3a1195bc3404a424f621158548cd01b33a
trevorturk.com <trevorturk.com@9c008b19-a030-0410-9975-d76d301b4276>
2006-12-18 02:39:28 +0000

    removing plugins that require ruby version greater than media temple
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@76 9c008b19-a030-0410-9975-d76d301b4276

--- vendor/plugins/bbcodeizer/lib/bbcodeize_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'bbcodeizer'
-
-module BBCodeizeHelper
-  # Parses all bbcode in +text+ and returns a new HTML-formatted string.
-  def bbcodeize(text)
-    BBCodeizer.bbcodeize(text)
-  end
-end
