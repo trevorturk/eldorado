5f21ceba1910d3123f6415808b8de9ceac2bd025
trevorturk.com <trevorturk.com@9c008b19-a030-0410-9975-d76d301b4276>
2007-03-19 03:17:22 +0000

    still trying to get uploads working (x4)
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@163 9c008b19-a030-0410-9975-d76d301b4276

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,60 +1,37 @@
 class FilesController < ApplicationController
-  # GET /files
-  # GET /files.xml
+
   def index
     @files = Files.find(:all)
-    flash[:notice] = "why?"
     respond_to do |format|
       format.html # index.rhtml
       format.xml  { render :xml => @files.to_xml }
     end
   end
 
-  # GET /files/1
-  # GET /files/1.xml
   def show
     @files = Files.find(params[:id])
-
     respond_to do |format|
       format.html # show.rhtml
       format.xml  { render :xml => @files.to_xml }
     end
   end
 
-  # GET /files/new
   def new
     @files = Files.new
   end
 
-  # GET /files/1;edit
   def edit
     @files = Files.find(params[:id])
   end
 
-  # POST /files
-  # POST /files.xml
   def create
-    flash[:notice] = "no"
-    @files = Files.new(params[:files])
-    respond_to do |format|
-      if @files.save
-        flash[:notice] = "yes"
-        format.html { redirect_to files_url(@files) }
-        format.xml  { head :created, :location => files_url(@files) }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @files.errors.to_xml }
-      end
-    end
   end
   
   def upload
-    flash[:notice] = "no"
     @files = Files.new(params[:files])
     respond_to do |format|
       if @files.save
-        flash[:notice] = "yes"
-        format.html { redirect_to files_url(@files) }
+        format.html { redirect_to :controller => "files" }
         format.xml  { head :created, :location => files_url(@files) }
       else
         format.html { render :action => "new" }
@@ -63,11 +40,8 @@ class FilesController < ApplicationController
     end
   end
 
-  # PUT /files/1
-  # PUT /files/1.xml
   def update
     @files = Files.find(params[:id])
-
     respond_to do |format|
       if @files.update_attributes(params[:files])
         format.html { redirect_to files_url(@files) }
@@ -79,12 +53,9 @@ class FilesController < ApplicationController
     end
   end
 
-  # DELETE /files/1
-  # DELETE /files/1.xml
   def destroy
     @files = Files.find(params[:id])
     @files.destroy
-
     respond_to do |format|
       format.html { redirect_to :controller => 'files' }
       format.xml  { head :ok }
