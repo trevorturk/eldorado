891413eb8d446be2fb5f11e36c73db13c9913076
trevorturk.com <trevorturk.com@9c008b19-a030-0410-9975-d76d301b4276>
2007-03-19 03:40:09 +0000

    still trying to get uploads working (x5)
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@164 9c008b19-a030-0410-9975-d76d301b4276

--- app/models/header.rb
+++ app/models/header.rb
@@ -4,9 +4,4 @@ class Header < ActiveRecord::Base
   validates_as_attachment
   validates_uniqueness_of :filename
 
-  def full_filename(thumbnail = nil)
-    file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
-    File.join(RAILS_ROOT, file_system_path, thumbnail_name_for(thumbnail))
-  end
-
 end
