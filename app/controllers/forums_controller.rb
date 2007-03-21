9efd7d8f27b32e932cb4f5858bd1b74356138f7b
trevorturk.com <trevorturk.com@9c008b19-a030-0410-9975-d76d301b4276>
2007-03-21 13:02:33 +0000

    redesign of online/stats area and footer
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@179 9c008b19-a030-0410-9975-d76d301b4276

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,41 +1,31 @@
 class ForumsController < ApplicationController
-  # GET /forums
-  # GET /forums.xml
+
   def index
     @forums = Forum.find(:all)
-
     respond_to do |format|
       format.html # index.rhtml
       format.xml  { render :xml => @forums.to_xml }
     end
   end
 
-  # GET /forums/1
-  # GET /forums/1.xml
   def show
     @forum = Forum.find(params[:id])
-
     respond_to do |format|
       format.html # show.rhtml
       format.xml  { render :xml => @forum.to_xml }
     end
   end
 
-  # GET /forums/new
   def new
     @forum = Forum.new
   end
 
-  # GET /forums/1;edit
   def edit
     @forum = Forum.find(params[:id])
   end
 
-  # POST /forums
-  # POST /forums.xml
   def create
     @forum = Forum.new(params[:forum])
-
     respond_to do |format|
       if @forum.save
         flash[:notice] = 'Forum was successfully created.'
@@ -48,11 +38,8 @@ class ForumsController < ApplicationController
     end
   end
 
-  # PUT /forums/1
-  # PUT /forums/1.xml
   def update
     @forum = Forum.find(params[:id])
-
     respond_to do |format|
       if @forum.update_attributes(params[:forum])
         flash[:notice] = 'Forum was successfully updated.'
@@ -65,12 +52,9 @@ class ForumsController < ApplicationController
     end
   end
 
-  # DELETE /forums/1
-  # DELETE /forums/1.xml
   def destroy
     @forum = Forum.find(params[:id])
     @forum.destroy
-
     respond_to do |format|
       format.html { redirect_to forums_url }
       format.xml  { head :ok }
