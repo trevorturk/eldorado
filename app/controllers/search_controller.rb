932d7524a253531ca3b14478d1a830bc97af722e
trevorturk.com <trevorturk.com@9c008b19-a030-0410-9975-d76d301b4276>
2007-04-07 04:31:35 +0000

    setting up paths for search and results
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@258 9c008b19-a030-0410-9975-d76d301b4276

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,11 @@
 class SearchController < ApplicationController
 
   def index
-    if request.post?
+    if !request.post?
+      render :template => "search/index"
+    else
       @topics = Topic.find(:all, :include => [:user, :last_poster], :order => 'last_post_at desc')
       render :template => "topics/index"
-    else
-      render :template => "home/search"
     end
   end
   
