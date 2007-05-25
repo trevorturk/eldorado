3645d81111cb0e7da3bc8dbb2babfdd223fdb020
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2007-05-25 23:52:21 +0000

    altering the rails time_ago_in_words method to show dates in a way I like better
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@308 9c008b19-a030-0410-9975-d76d301b4276

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -80,5 +80,23 @@ module ApplicationHelper
   def current_action
     request.path_parameters['action']
   end
+  
+  def distance_of_time_in_words(from_time, to_time = 0, include_seconds = true)
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    to_time = to_time.to_time if to_time.respond_to?(:to_time)
+    distance_in_minutes = (((to_time - from_time).abs)/60).round
+    distance_in_seconds = ((to_time - from_time).abs).round
+
+    case distance_in_minutes
+      when 0..1           then (distance_in_seconds < 60) ? "#{distance_in_seconds} seconds ago" : '1 minute ago'
+      when 2..59          then "#{distance_in_minutes} minutes ago"
+      when 60..90         then "1 hour ago"
+      when 90..1440       then "#{(distance_in_minutes.to_f / 60.0).round} hours ago"
+      when 1440..2160     then '1 day ago' # 1 day to 1.5 days
+      when 2160..2880     then "#{(distance_in_minutes.to_f / 1440.0).round} days ago" # 1.5 days to 2 days
+      when 2880..1051199  then from_time.strftime("%a, %d %b") # up to 1 year
+      else from_time.strftime("%d %b, %Y")
+    end
+  end
       
 end
