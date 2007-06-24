862ffab88792c1b62955ea383af4821f191be33f
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2007-06-24 01:55:02 +0000

    adding search, deleting unused layouts
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@327 9c008b19-a030-0410-9975-d76d301b4276

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 class SearchController < ApplicationController
-
+  
+  before_filter :force_login
+  
   def index
-    if !request.post?
-      render :template => "search/index"
-    else
-      @topics = Topic.paginate(:page => params[:page], :include => [:user, :last_poster], :order => 'last_post_at desc')
+    if request.post?
+      @topics = Topic.paginate(:page => params[:page], :include => [:user, :last_poster], :order => 'last_post_at desc', :conditions => ["title LIKE ?", '%' + params[:q] + '%'])
       render :template => "topics/index"
+    else
+      render :template => "search/index"
     end
   end
   
