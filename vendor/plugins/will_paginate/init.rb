dccec992b19dddab9facd14f5e0997905673eff9
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2007-07-26 01:01:53 +0000

    updaing will_paginate plugin
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@379 9c008b19-a030-0410-9975-d76d301b4276

--- vendor/plugins/will_paginate/init.rb
+++
@@ -1,6 +0,0 @@
-require 'will_paginate/collection'
-require 'will_paginate/finder'
-require 'will_paginate/view_helpers'
-
-ActiveRecord::Base.send :include, WillPaginate::Finder
-ActionView::Base.send   :include, WillPaginate::ViewHelpers
