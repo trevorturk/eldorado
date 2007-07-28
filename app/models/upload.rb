af3f6e3b8c3b19e0cd708f1df41bf5ffca7ee890
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2007-07-28 21:42:59 +0000

    upping the file upload limit
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@399 9c008b19-a030-0410-9975-d76d301b4276

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -20,7 +20,7 @@ class Upload < ActiveRecord::Base
   
   belongs_to :user
   
-  has_attachment :storage => :file_system, :path_prefix => 'public/files', :max_size => 50.megabytes
+  has_attachment :storage => :file_system, :path_prefix => 'public/files', :max_size => 100.megabytes
   
   validates_as_attachment
   validates_uniqueness_of :filename
