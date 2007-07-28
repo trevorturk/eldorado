9dea9f1fc6ed03fb2c3f9bf343f8339c106ffc55
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2007-07-28 18:53:06 +0000

    over-riding public_filename to allow un-sanatized old files to work
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@396 9c008b19-a030-0410-9975-d76d301b4276

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -32,5 +32,9 @@ class Upload < ActiveRecord::Base
     file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
     File.join(RAILS_ROOT, file_system_path, thumbnail_name_for(thumbnail))
   end
-      
+
+  def public_filename
+    'files/' + filename
+  end
+
 end
