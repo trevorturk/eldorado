5a6c513b59f0c1e139a85c5da8e1f1dc2878c934
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2007-10-12 03:08:27 +0000

    starting work on more comprehensive home page
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@479 9c008b19-a030-0410-9975-d76d301b4276

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
   
   def index
     if logged_in?
-      @topics = Topic.paginate(:page => params[:page], :per_page => Topic::PER_PAGE, :include => [:user, :forum, :last_poster], :order => 'topics.last_post_at desc')
-      render(:template => "topics/index")
+      @topics = Topic.paginate(:page => params[:page], :per_page => 10, :include => [:user, :forum, :last_poster], :order => 'topics.last_post_at desc')
+      @date = Time.parse("#{params[:date]} || TzTime.now")
+      @events = Event.paginate(:page => params[:page], :per_page => Topic::PER_PAGE, :order => 'updated_at desc')
     else
       render(:template => "users/login")
     end
