80f2164b5e98f73d280fb1fc578674141ccc9951
iamwil <iamwil@9c008b19-a030-0410-9975-d76d301b4276>
2008-01-07 04:39:36 +0000

    fixed broken spoiler and nsfw bbcode blocks over multiple lines
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@600 9c008b19-a030-0410-9975-d76d301b4276

--- vendor/plugins/bbcodeizer/lib/bbcodeizer.rb
+++ vendor/plugins/bbcodeizer/lib/bbcodeizer.rb
@@ -1,5 +1,9 @@
 module BBCodeizer  
   class << self
+    # generates a random id for AJAXified tags
+    def random_id
+      [Array.new(6){rand(256).chr}.join].pack("m").chomp
+    end
 
     #:nodoc:
     Tags = {
@@ -22,8 +26,10 @@ module BBCodeizer
       :youtube               => [ /\[youtube\](.+?)youtube.com\/watch\?v=(.+?)\[\/youtube\]/i, '<object width="425" height="350"><param name="movie" value="http://www.youtube.com/v/\2"></param><embed src="http://www.youtube.com/v/\2" type="application/x-shockwave-flash" width="425" height="350"></embed></object>' ],
       :googlevid             => [ /\[googlevid\](.+?)video.google.com\/videoplay\?docid=(.*?)\[\/googlevid\]/i, '<embed style="width:400px; height:326px;" id="VideoPlayback" type="application/x-shockwave-flash" src="http://video.google.com/googleplayer.swf?docId=\2&amp;hl=en"></embed>' ],
       :flash                 => [ /\[flash\](.+?)\[\/flash\]/i, '<object width="100%" height="100%"><param name="movie" value="\1"></param><embed src="\1" type="application/x-shockwave-flash" width="100%" height="100%"></embed></object>' ],
-      :spoiler               => [ /\[spoiler\](.+?)\[\/spoiler\]/im, '<a href="#" class="spoiler-link" onclick="$(\'_SPOILER\').toggle(); return false;">SPOILER</a><div id="_SPOILER" class="spoiler" style="display:none;">\1</div>' ],
-      :nsfw                  => [ /\[nsfw\](.+?)\[\/nsfw\]/i, '<a href="#" class="nsfw-link" onclick="$(\'_NSFW\').toggle(); return false;">NSFW</a><div id="_NSFW" class="nsfw" style="display:none;">\1</div>' ],
+      :start_spoiler         => [ /\[spoiler\]/i, proc { id = BBCodeizer::random_id(); %Q{ <a href="#" class="spoiler-link" onclick="$(\'#{id}\').toggle(); return false;">SPOILER</a><div id="#{id}" class="spoiler" style="display:none;"> } } ],
+      :end_spoiler           => [ /\[\/spoiler\]/i, '</div>' ],
+      :start_nsfw            => [ /\[nsfw\]/i, proc { id = BBCodeizer::random_id(); %Q{ <a href="#" class="nsfw-link" onclick="$(\'#{id}\').toggle(); return false;">NSFW</a><div id="#{id}" class="nsfw" style="display:none;"> } } ],
+      :end_nsfw              => [ /\[\/nsfw\]/i, '</div>' ],
       :mp3                   => [ /\[mp3\](.+?)\[\/mp3\]/i, '<script language="JavaScript" src="/javascripts/audio-player.js"></script><object type="application/x-shockwave-flash" data="/flash/player.swf" id="_MP3" height="24" width="290"><param name="movie" value="/flash/player.swf"><param name="FlashVars" value="playerID=_MP3&amp;soundFile=\1"><param name="quality" value="high"><param name="menu" value="false"><param name="wmode" value="transparent"></object>' ],
       :superdeluxe           => [ /\[superdeluxe\](.+?)superdeluxe.com\/sd\/contentDetail.do\?id=(.+?)\[\/superdeluxe\]/i, '<object width="400" height="350"><param name="allowFullScreen" value="true" /><param name="movie" value="http://www.superdeluxe.com/static/swf/share_vidplayer.swf" /><param name="FlashVars" value="id=\2" /><embed src="http://www.superdeluxe.com/static/swf/share_vidplayer.swf" FlashVars="id=\2" type="application/x-shockwave-flash" width="400" height="350" allowFullScreen="true" ></embed></object>' ],
       :comedycentral         => [ /\[comedycentral\](.+?)comedycentral.com\/motherload\/index.jhtml\?ml_video=(.+?)\[\/comedycentral\]/i, '<embed FlashVars="config=http://www.comedycentral.com/motherload/xml/data_synd.jhtml?vid=\2%26myspace=false" src="http://www.comedycentral.com/motherload/syndicated_player/index.jhtml" quality="high" bgcolor="#006699" width="340" height="325" name="comedy_player" align="middle" allowScriptAccess="always" allownetworking="external" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed>' ],
@@ -53,18 +59,10 @@ module BBCodeizer
         else
           self.send(tag, text)
         end
-        char = ("a".."z").to_a + ("1".."9").to_a 
-        random_string = Array.new(6, '').collect{char[rand(char.size)]}.join
-        text = text.sub('_SPOILER', random_string)
-        text = text.sub('_SPOILER', random_string)
-        char = ("a".."z").to_a + ("1".."9").to_a 
-        random_string = Array.new(6, '').collect{char[rand(char.size)]}.join
-        text = text.sub('_MP3', random_string)
-        text = text.sub('_MP3', random_string)
-        char = ("a".."z").to_a + ("1".."9").to_a 
-        random_string = Array.new(6, '').collect{char[rand(char.size)]}.join
-        text = text.sub('_NSFW', random_string)
-        text = text.sub('_NSFW', random_string)
+
+        ['_MP3'].each do |tag_id_name|
+          text.gsub!(tag_id_name, random_id())
+        end
       end
       text
     end
@@ -81,24 +79,47 @@ module BBCodeizer
     end
     
   private
+    # code tags must match, else don't do any replacing.
+    def on_matching_tags(tag_name, string)
+      if string.scan(Tags[:"start_#{tag_name}"].first).size == string.scan(Tags[:"end_#{tag_name}"].first).size
+        yield
+      end
+    end
 
     def code(string)
-      # code tags must match, else don't do any replacing.
-      if string.scan(Tags[:start_code].first).size == string.scan(Tags[:end_code].first).size
+      on_matching_tags(:code, string) do
         apply_tags(string, :start_code, :end_code)
       end
     end
   
     def quote(string)
-      # quotes must match, else don't do any replacing
-      if string.scan(Tags[:start_quote].first).size == string.scan(Tags[:end_quote].first).size
+      on_matching_tags(:quote, string) do
         apply_tags(string, :start_quote_with_cite, :start_quote_sans_cite, :end_quote)
+      end
+    end
+
+    def spoiler(string)
+      on_matching_tags(:spoiler, string) do
+        apply_tags(string, :start_spoiler, :end_spoiler)
+      end
+    end
+
+    def nsfw(string)
+      on_matching_tags(:nsfw, string) do
+        apply_tags(string, :start_nsfw, :end_nsfw)
       end
     end
 
     def apply_tags(string, *tags)
       tags.each do |tag|
-        string.gsub!(*Tags[tag])
+        regex, substitution = Tags[tag]
+        if substitution.kind_of?(String)
+          string.gsub!(*Tags[tag])
+        elsif substitution.kind_of?(Proc)
+          while string =~ regex
+            string.sub!(regex, substitution.call)
+          end
+        end
       end
     end
     alias_method :apply_tag, :apply_tags
