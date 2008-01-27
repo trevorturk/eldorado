ffe59118ed18cbf0b574bf2fea534dca2bd24f71
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2008-01-27 19:13:09 +0000

    adding version 0.9.1 tag
    git-svn-id: http://eldorado.googlecode.com/svn/tags/rel-0.9.1@638 9c008b19-a030-0410-9975-d76d301b4276

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION   = '2.0.2' unless defined? RAILS_GEM_VERSION
+APPLICATION_VERSION = '0.9.1'
 
 require File.join(File.dirname(__FILE__), 'boot')
 
