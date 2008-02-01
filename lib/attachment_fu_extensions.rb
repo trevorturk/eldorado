f32eddfdad1fbdd5d8584ec074d99058abe719f6
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2008-02-01 06:10:10 +0000

    adding some comments to explain the attachment_fu shared code a bit
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@645 9c008b19-a030-0410-9975-d76d301b4276

--- lib/attachment_fu_extensions.rb
+++ lib/attachment_fu_extensions.rb
@@ -1,5 +1,6 @@
 module AttachmentFuExtensions
   
+  # include some standard attachment_fu calls
   def self.included(klass)
     klass.send :validates_as_attachment
     klass.send :validates_uniqueness_of, :filename
@@ -7,13 +8,15 @@ module AttachmentFuExtensions
     klass.send :attr_protected, :id, :parent_id, :user_id, :created_at, :updated_at
   end
   
-  def validate
-    errors.add("filename", "is invalid") if %w(index.html index.htm).include?(filename.downcase)
-  end
-  
+  # upload into a single directory (e.g. public/uploads) instead of subdirectories based on the id
   def full_filename(thumbnail = nil)
     file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
     File.join(RAILS_ROOT, file_system_path, thumbnail_name_for(thumbnail))
   end
-
+  
+  # prevent users from uploading index files that would be served instead of your the template/action
+  def validate
+    errors.add("filename", "is invalid") if %w(index.html index.htm).include?(filename.downcase)
+  end
+  
 end
