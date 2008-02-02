76ebb073bb6edd5fd3192e51cd9ed8e6182054e7
trevorturk <trevorturk@9c008b19-a030-0410-9975-d76d301b4276>
2008-02-02 05:32:29 +0000

    adding migration version checker from advanced rails recipes pdf
    git-svn-id: http://eldorado.googlecode.com/svn/trunk@649 9c008b19-a030-0410-9975-d76d301b4276

---
+++ config/initializers/check_migration_version.rb
@@ -0,0 +1,12 @@
+# Prevent application from starting with incorrect database schema version (via http://pragprog.com/titles/fr_arr)
+current_version = ActiveRecord::Migrator.current_version rescue 0
+
+highest_version = Dir.glob("#{RAILS_ROOT}/db/migrate/*.rb").map { |f|
+  f.match(/(\d+)_.*\.rb$/) ? $1.to_i : 0
+}.max
+
+unless defined?(Rake) # skip when run from tasks like rake db:migrate
+  if current_version != highest_version
+    abort "Expected migration version #{highest_version}, got #{current_version}. Run 'rake db:migrate' to upgrade schema."
+  end
+end
