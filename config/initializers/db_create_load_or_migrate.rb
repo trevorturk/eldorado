42b54809f38f7ccf92841720cf700e47be879c05
Trevor Turk <trevorturk@yahoo.com>
2008-04-09 18:19:07 -0500

    creating database a la database.yml if it doesnt exist

--- config/initializers/db_create_load_or_migrate.rb
+++ config/initializers/db_create_load_or_migrate.rb
@@ -1,4 +1,4 @@
-# Load initial schema or migrate database if necessary (via http://pragprog.com/titles/fr_arr)
+# Create database, load initial schema, or migrate as necessary (via http://pragprog.com/titles/fr_arr)
 unless defined?(Rake) # skip when loaded from rake tasks
   load "#{RAILS_ROOT}/Rakefile"
   begin
@@ -6,6 +6,7 @@ unless defined?(Rake) # skip when loaded from rake tasks
     highest_version = Dir.glob("#{RAILS_ROOT}/db/migrate/*.rb").map { |f| f.match(/(\d+)_.*\.rb$/) ? $1.to_i : 0 }.max
     Rake::Task["db:migrate"].invoke if current_version != highest_version
   rescue
+    Rake::Task["db:create"].invoke
     abort 'ERROR: Database has no schema version and is not empty' unless ActiveRecord::Base.connection.tables.blank?
     Rake::Task["db:schema:load"].invoke
     retry
