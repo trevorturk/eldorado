9dfd7b9408906d2b98e0e7aacbe39e3eaeb905dd
Trevor Turk <trevorturk@yahoo.com>
2008-04-23 10:42:27 -0500

    small patch for check_migration to allow directory names with digits followed by an underscore [bmishkin]

--- config/initializers/db_create_load_or_migrate.rb
+++ config/initializers/db_create_load_or_migrate.rb
@@ -3,7 +3,7 @@ unless defined?(Rake) # skip when loaded from rake tasks
   load "#{RAILS_ROOT}/Rakefile"
   begin
     current_version = ActiveRecord::Migrator.current_version
-    highest_version = Dir.glob("#{RAILS_ROOT}/db/migrate/*.rb").map { |f| f.match(/(\d+)_.*\.rb$/) ? $1.to_i : 0 }.max
+    highest_version = Dir.glob("#{RAILS_ROOT}/db/migrate/*.rb").map { |f| f.match(/(\d+)_\w*\.rb$/) ? $1.to_i : 0 }.max
     Rake::Task["db:migrate"].invoke if current_version != highest_version
   rescue
     Rake::Task["db:create"].invoke
