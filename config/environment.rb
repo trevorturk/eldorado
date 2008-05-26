f34a7e6ea2b7df1d579d01084b119c8229b45bce
Trevor Turk <trevorturk@yahoo.com>
2008-05-26 13:58:20 -0500

    cleaning up environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,21 +1,16 @@
-APPLICATION_VERSION = '0.9.3 (beta)'
-
 require File.join(File.dirname(__FILE__), 'boot')
 
-Rails::Initializer.run do |config|
-  
-  # Require libraries
+Rails::Initializer.run do |config|  
   require 'open-uri'
   require 'yaml'
   
-  # Invoke Rails 2.1 Timezone functionality 
   config.time_zone = 'UTC'
-    
-  # The session_key and secret (for verifying session data integrity) can be set in config/database.yml
+  config.active_record.partial_updates = true
+  
+  # The session_key and secret (for verifying session data integrity) are set in config/database.yml
   db = YAML.load_file('config/database.yml')
   config.action_controller.session = {
     :session_key => db[RAILS_ENV]['session_key'],
     :secret      => db[RAILS_ENV]['secret']
   }
-  
 end
--- config/initializers/application.rb
+++
@@ -1,2 +0,0 @@
-# Only save the attributes that have changed since the record was loaded.
-ActiveRecord::Base.partial_updates = true
