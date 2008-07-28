3f377c43f57a01d007bcb5c10719a5179558f37a
Trevor Turk <trevorturk@yahoo.com>
2008-07-28 01:11:13 -0500

    basic commenting enabled with show/edit/update actions and views in place

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   
-  before_filter :redirect_home, :except => [:create]
+  before_filter :redirect_home, :only => [:index, :new, :destroy]
   before_filter :require_login, :except => [:index, :show]
   before_filter :can_edit, :only => [:edit, :update, :destroy]
   
@@ -8,24 +8,32 @@ class CommentsController < ApplicationController
   end
   
   def show
+    @comment = Comment.find(params[:id])
   end
   
   def new
   end
   
   def edit
+    @comment = Comment.find(params[:id])
   end
   
   def create
     @comment = current_user.comments.build(params[:comment])
     if @comment.save
-      redirect_to polymorphic_url(@comment.resource) + '#c' + @comment.id.to_s
+      redirect_to polymorphic_path(@comment.resource) + '#c' + @comment.id.to_s
     else
-      redirect_to polymorphic_url(@comment.resource) + '#comments' + @comment.id.to_s
+      redirect_to polymorphic_path(@comment.resource) + '#comments' + @comment.id.to_s
     end
   end
   
   def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(params[:comment])
+      redirect_to polymorphic_path(@comment.resource) + '#c' + @comment.id.to_s
+    else
+      render :action => "edit"
+    end
   end
   
   def destroy
