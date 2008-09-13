6583be6c98d7385ec85fc72cc7a8a42cea4272cc
Trevor Turk <trevorturk@yahoo.com>
2008-09-13 17:55:18 -0500

    adding cleanup task to deploy example

--- config/deploy.example.rb
+++ config/deploy.example.rb
@@ -16,6 +16,7 @@ role :db,  "000.00.00.000", :primary => true
 before  'deploy:update_code', 'deploy:web:disable'
 after   'deploy:update_code', 'deploy:config_database'
 after   'deploy:update_code', 'deploy:create_symlinks'
+after   'deploy:restart', 'deploy:cleanup'
 after   'deploy:restart', 'deploy:web:enable'
 
 namespace :deploy do
