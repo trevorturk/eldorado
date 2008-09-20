0c210de7cab771fd58f9c7105d07e92c6144311b
Trevor Turk <trevorturk@yahoo.com>
2008-09-20 13:31:29 -0500

    adding testing for valid fixtures, making sure all fixtures are valid

---
+++ test/integration/fixture_validation_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class FixtureValidationTest < ActionController::IntegrationTest
+
+  def test_fixtures_should_be_valid
+    models = Fixtures.all_loaded_fixtures.keys
+    models.each do |model|
+      model = model.capitalize.singularize.constantize
+      fixtures = model.find(:all)
+      fixtures.each do |fixture|
+        if !fixture.valid?
+          puts; puts "WARNING: Invalid fixture: #{fixture.inspect}"
+        end
+        assert_valid fixture
+      end
+    end
+  end
+end
