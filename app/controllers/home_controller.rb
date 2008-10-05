bcb7badb097f5774302a4b0f7fdbc3015e6496c3
Trevor Turk <trevorturk@yahoo.com>
2008-10-05 16:40:38 -0500

    whoops :)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
       @chatters = User.chatting
       @events = Event.find(:all, :conditions => ['date between ? and ?', @date.strftime("%Y-%m") + '-01', @date.next_month.strftime("%Y-%m") + '-01'])
       @headers = Header.find(:all, :limit => 3, :include => :user, :order => 'headers.created_at desc')
-      @topics = Topic.get(1, 1)
+      @topics = Topic.get(1, 20)
       @uploads = Upload.find(:all, :limit => 5, :include => :user, :order => 'uploads.updated_at desc')
       @users = User.find(:all, :limit => 3, :order => 'profile_updated_at desc')
   end
