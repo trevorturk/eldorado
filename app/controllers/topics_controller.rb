876a53dbcc8a2cd8774cbc2259cef6f4399a820f
Trevor Turk <trevorturk@yahoo.com>
2008-10-04 22:32:15 -0500

    hiding mark all read if not logged in, adding flash message to mark all read

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -72,6 +72,7 @@ class TopicsController < ApplicationController
   
   def mark_all_viewed
     current_user.update_attribute(:all_viewed_at, Time.now)
+    flash[:notice] = "All topics marked as read"
     redirect_to forum_root_path
   end
   
