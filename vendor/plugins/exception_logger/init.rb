f9f9b19e56f387053a7863a2b1b1eb2e263c6d16
Trevor Turk <trevorturk@yahoo.com>
2008-12-03 22:35:37 -0600

    update exception logger plugin

--- vendor/plugins/exception_logger/init.rb
+++ vendor/plugins/exception_logger/init.rb
@@ -1,3 +1,7 @@
-require 'will_paginate' unless Kernel.const_defined? 'WillPaginate'
-WillPaginate.enable
+	$PAGINATION_TYPE = 'none'
+	#require 'will_paginate'
+	#$PAGINATION_TYPE = 'will_paginate'
+	#WillPaginate.enable
+  #require 'paginating_find'
+	#$PAGINATION_TYPE = 'paginating_find'
 LoggedExceptionsController.view_paths = [File.join(directory, 'views')]
--- vendor/plugins/exception_logger/lib/logged_exceptions_helper.rb
+++ vendor/plugins/exception_logger/lib/logged_exceptions_helper.rb
@@ -4,21 +4,42 @@ module LoggedExceptionsHelper
   end
 
   def pagination_remote_links(collection)
-    will_paginate collection, 
-      :renderer   => 'LoggedExceptionsHelper::PaginationRenderer',
-      :prev_label => '',
-      :next_label => '',
-      :container  => false
+		ret = ''
+		if $PAGINATION_TYPE == 'will_paginate'
+			pagination = will_paginate (collection, 
+				:renderer   => 'LoggedExceptionsHelper::PaginationRenderer',
+				:prev_label => '',
+				:next_label => '',
+				:container  => false)
+			if collection.total_pages > 1 then
+				ret = ":: Pages : <strong>#{pagination}</strong>"
+			end
+		elsif $PAGINATION_TYPE == 'paginating_find' then
+			pagination = paginating_links collection
+			ret = ":: Pages : <strong>#{pagination}</strong>"
+		else
+			next_page = params[:page].to_i + 1
+			prev_page = 0
+			prev_link = ''
+			if params[:page].to_i > 0 then
+				prev_page = params[:page].to_i - 1
+				prev_link = "<a href=\"?page=#{prev_page}\"> Previous page</a>"
+			end
+			next_link = "<a href=\"?page=#{next_page}\">Next page</a>"		
+			ret = "Pagination not available#{prev_link} - #{next_link}"
+		end
   end
-
-  class PaginationRenderer < WillPaginate::LinkRenderer
-    def page_link_or_span(page, span_class = 'current', text = nil)
-      text ||= page.to_s
-      if page and page != current_page
-        @template.link_to_function text, "ExceptionLogger.setPage(#{page})"
-      else
-        @template.content_tag :span, text, :class => span_class
-      end
-    end
-  end
+	
+  if $PAGINATION_TYPE == 'will_paginate'
+		class PaginationRenderer < WillPaginate::LinkRenderer
+			def page_link_or_span(page, span_class = 'current', text = nil)
+				text ||= page.to_s
+				if page and page != current_page
+					@template.link_to_function text, "ExceptionLogger.setPage(#{page})"
+				else
+					@template.content_tag :span, text, :class => span_class
+				end
+			end
+		end
+	end
 end
