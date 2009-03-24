1364de06e93d230a7f056083c67c17c78ad3465c
Trevor Turk <trevorturk@yahoo.com>
2009-03-23 20:45:01 -0500

    update calendar_date_select

--- vendor/plugins/calendar_date_select/lib/calendar_date_select/calendar_date_select.rb
+++ vendor/plugins/calendar_date_select/lib/calendar_date_select/calendar_date_select.rb
@@ -1,5 +1,5 @@
 module CalendarDateSelect
-  VERSION = '1.13'
+  VERSION = '1.15'
   FORMATS = {
     :natural => {
       :date => "%B %d, %Y",
--- vendor/plugins/calendar_date_select/spec/calendar_date_select/form_helpers_spec.rb
+++ vendor/plugins/calendar_date_select/spec/calendar_date_select/form_helpers_spec.rb
@@ -124,7 +124,16 @@ describe CalendarDateSelect::FormHelpers do
     output.should_not include("image:")
   end
 
+  it "should use the CSS class calendar_date_select_tag for popup selector icon" do
+    output = calendar_date_select_tag(:name, "Some String", :image => "boogy.png")
+    output.should include("calendar_date_select_popup_icon")
+  end
+
   describe "calendar_date_select_tag" do
+    before(:each) do
+      @time = Time.parse("January 2, 2007 12:01:23 AM")
+    end
+    
     it "should use the string verbatim when provided" do
       output = calendar_date_select_tag(:name, "Some String")
 
@@ -132,25 +141,26 @@ describe CalendarDateSelect::FormHelpers do
     end
 
     it "should not render the time when time is false (or nil)" do
-      time = Time.parse("January 2, 2007 12:01:23 AM")
-      output = calendar_date_select_tag(:name, time, :time => false)
+      output = calendar_date_select_tag(:name, @time, :time => false)
 
       output.should_not match(/12:01 AM/)
-      output.should include(CalendarDateSelect.format_date(time.to_date))
+      output.should include(CalendarDateSelect.format_date(@time.to_date))
     end
 
     it "should render the time when :time => true" do
-      time = Time.parse("January 2, 2007 12:01:23 AM")
-      output = calendar_date_select_tag(:name, time, :time => true)
+      output = calendar_date_select_tag(:name, @time, :time => true)
 
-      output.should include(CalendarDateSelect.format_date(time))
+      output.should include(CalendarDateSelect.format_date(@time))
     end
 
     it "should render the time when :time => 'mixed'" do
-      time = Time.parse("January 2, 2007 12:01:23 AM")
-      output = calendar_date_select_tag(:name, time, :time => 'mixed')
-
-      output.should include(CalendarDateSelect.format_date(time))
+      output = calendar_date_select_tag(:name, @time, :time => 'mixed')
+      output.should include(CalendarDateSelect.format_date(@time))
+    end
+    
+    it "not include the image option in the result input tag" do
+      output = calendar_date_select_tag(:name, @time, :time => 'mixed')
+      output.should_not include("image=")
     end
   end
 end
