84b636cdf969e1237559f4b7f748cc2022a5c145
Trevor Turk <trevorturk@yahoo.com>
2009-06-27 15:19:27 -0500

    small refactoring of attachment_fu extensions

--- lib/attachment_fu_extensions.rb
+++ lib/attachment_fu_extensions.rb
@@ -2,15 +2,15 @@ module AttachmentFuExtensions
     
   # include some standard attachment_fu stuff
   def self.included(klass)
+    klass.send :attr_protected, :id, :parent_id, :user_id, :created_at, :updated_at
     klass.send :validates_as_attachment
     klass.send :validates_uniqueness_of, :filename
-    klass.send :validates_presence_of, :user_id
-    klass.send :attr_protected, :id, :parent_id, :user_id, :created_at, :updated_at
+    klass.send :validates_presence_of, :user_id, :filename
+    klass.send :validate, :disallow_uploading_files_that_would_be_served_instead_of_index_action
   end
   
-  # prevent users from uploading index files that would be served instead of the template/action
-  def validate
-    errors.add("filename", "is invalid") if filename? && %w(index.html index.htm).include?(filename.downcase)
+  def disallow_uploading_files_that_would_be_served_instead_of_index_action
+    errors.add(:filename) if filename && %w(index.htm index.html).include?(filename.downcase)
   end
   
   # allow uploads via URL
