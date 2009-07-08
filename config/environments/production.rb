6325f5e9ac88e4f23fb4c66e825f8b52e1bf0e06
Trevor Turk <trevorturk@yahoo.com>
2009-07-07 22:42:23 -0500

    email support for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching = true
 config.action_view.cache_template_loading = true
+config.action_mailer.raise_delivery_errors = true
 
 config.after_initialize do
   require 'application_controller' unless Object.const_defined?(:ApplicationController)
@@ -11,4 +12,16 @@ config.after_initialize do
     include AuthenticationSystem
     before_filter :require_admin
   end
+end
+
+if CONFIG['smtp']
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.server_settings = {
+    :address => CONFIG['smtp_address'],
+    :port => CONFIG['smtp_port'], 
+    :domain => CONFIG['smtp_domain'],
+    :user_name => CONFIG['smtp_user_name'],
+    :password => CONFIG['smtp_password'],
+    :authentication => CONFIG['smtp_authentication']
+  }
 end
