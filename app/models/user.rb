0927924b461d6b3004e4b68be0cea0b7bbf4785e
Trevor Turk <trevorturk@gmail.com>
2010-02-09 13:48:49 -0600

    fix user banning

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
-    
-  attr_accessible :login, :email, :password, :password_confirmation, :avatar, :signature, :bio, :time_zone
+
+  attr_accessible :login, :email, :password, :password_confirmation, :avatar, :signature, :bio, :time_zone, :ban_message, :banned_until
   attr_reader :password
-  
+
   has_many :articles, :dependent => :destroy, :order => 'created_at desc'
   has_many :avatars, :dependent => :destroy, :order => 'created_at desc'
   has_many :comments, :dependent => :destroy, :order => 'created_at desc'
@@ -23,46 +23,46 @@ class User < ActiveRecord::Base
       if include?(topic_id)
         find_by_topic_id(topic_id.to_i).destroy
       else
-        create :topic_id => topic_id 
+        create :topic_id => topic_id
       end
     end
-    
+
     def include?(topic_or_id)
       exists? :topic_id => topic_or_id.is_a?(Topic) ? topic_or_id.id : topic_or_id.to_i
     end
   end
-  
+
   validates_presence_of     :login, :email, :password_hash
   validates_uniqueness_of   :login, :case_sensitive => false
   validates_length_of       :login, :maximum => 25
   validates_format_of       :email, :on => :create, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   validates_confirmation_of :password, :on => :create
   validates_confirmation_of :password, :on => :update, :allow_blank => true
-  
+
   before_create :set_defaults
   before_create :make_admin_if_first_user
-    
+
   named_scope :blog_authors, :conditions => 'articles_count > 0', :order => 'articles_count desc'
   named_scope :chatting, lambda {|*args| {:conditions => ['chatting_at > ?', Time.now.utc-30.seconds], :order => 'login asc'}}
   named_scope :online, lambda {|*args| {:conditions => ['logged_out = ? and (online_at > ? or chatting_at > ?)', false, Time.now.utc-5.minutes, Time.now.utc-30.seconds], :order => 'login asc'}}
-  
+
   def updated_at
     profile_updated_at
   end
-    
+
   def is_online?
     return true if online_at > Time.now.utc-5.minutes unless online_at.nil?
   end
-  
+
   def banned?
     return true if banned_until > Time.now.utc unless banned_until.nil?
   end
-  
+
   def remove_ban
     self.ban_message = self.banned_until = nil
     self.save
   end
-  
+
   def select_avatar(new_avatar)
     if !new_avatar.current_avatar_user
       self.clear_avatar
@@ -70,7 +70,7 @@ class User < ActiveRecord::Base
       new_avatar.update_attribute(:current_user_id, self.id)
     end
   end
-    
+
   def clear_avatar
     current_avatar = Avatar.find_by_current_user_id(self.id)
     if current_avatar
@@ -78,36 +78,36 @@ class User < ActiveRecord::Base
       current_avatar.update_attribute(:current_user_id, nil)
     end
   end
-  
+
   def password=(value)
     return if value.blank?
     write_attribute :password_hash, User.encrypt(value)
     @password = value
   end
-  
+
   def self.authenticate(login, password)
     find_by_login_and_password_hash(login, encrypt(password))
   end
-  
+
   def self.encrypt(password)
     Digest::SHA1.hexdigest(password)
   end
-    
+
   def set_defaults
     self.profile_updated_at = self.all_viewed_at = Time.now.utc
     self.time_zone = Setting.find(:first).time_zone
   end
-  
+
   def make_admin_if_first_user
     self.admin = true if User.count == 0
   end
-  
+
   def to_s
     login
   end
-  
+
   def to_param
     "#{id}-#{to_s.parameterize}"
   end
-  
+
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  
+
   def test_index
-    get :index 
+    get :index
     assert_response :success
   end
 
@@ -18,25 +18,25 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
     assert_template 'new'
   end
-  
+
   def test_should_not_be_able_to_edit_wrong_user
     login_as :trevor
     get :edit, :id => 2
     assert_redirected_to root_path
   end
-  
+
   def test_should_not_be_able_to_edit_if_not_logged_in
     get :edit, :id => 2
     assert_redirected_to root_path
   end
-  
+
   def test_should_not_be_able_to_update_if_not_logged_in
     put :update, :id => 4, :user => { :bio => "ok!" }
     assert_redirected_to root_path
     users(:trevor).reload
     assert_not_equal "ok!", users(:trevor).bio
   end
-  
+
   def test_should_be_able_to_edit_self
     login_as :trevor
     get :edit, :id => 4
@@ -44,7 +44,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_template 'edit'
     assert_not_nil assigns(:user)
   end
-  
+
   def test_should_update_user_if_admin
     login_as :Administrator
     put :update, :id => 4, :user => { :bio => "ok!" }
@@ -52,7 +52,7 @@ class UsersControllerTest < ActionController::TestCase
     users(:trevor).reload
     assert_equal "ok!", users(:trevor).bio
   end
-  
+
   def test_should_update_user_if_self
     login_as :trevor
     put :update, :id => 4, :user => { :bio => "ok!" }
@@ -60,7 +60,7 @@ class UsersControllerTest < ActionController::TestCase
     users(:trevor).reload
     assert_equal "ok!", users(:trevor).bio
   end
-  
+
   def test_should_not_be_able_to_make_self_admin_if_not_admin
     login_as :trevor
     assert_raises RuntimeError do
@@ -75,7 +75,7 @@ class UsersControllerTest < ActionController::TestCase
       post :create, :user => { :login => "notadmin", :email => "test@aol.com", :password => 'test', :password_confirmation => 'test', :admin => true }
     end
   end
-  
+
   def test_should_not_update_user_if_not_authorized
     login_as :trevor
     put :update, :id => 2, :user => { :bio => "ok!" }
@@ -83,7 +83,7 @@ class UsersControllerTest < ActionController::TestCase
     users(:trevor).bio
     assert_equal "admin", users(:Administrator).bio
   end
-    
+
   def test_should_be_able_to_edit_any_user_if_admin
     login_as :Administrator
     get :edit, :id => 3
@@ -91,7 +91,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_template 'edit'
     assert_not_nil assigns(:user)
   end
-  
+
   def test_should_not_update_user_if_no_email
     login_as :trevor
     put :update, :id => 4, :user => { :login => "won't work", :email => "" }
@@ -99,14 +99,14 @@ class UsersControllerTest < ActionController::TestCase
     users(:trevor).reload
     assert_equal 'trevor', users(:trevor).login
   end
-  
+
   def test_should_allow_user_to_change_password_password_change
     login_as :trevor
     put :update, :id => 4, :user => { :password => "ok", :password_confirmation => "ok" }
     users(:trevor).reload
     assert_equal User.encrypt('ok'), users(:trevor).password_hash
   end
-  
+
   def test_should_enforce_password_confirmation_on_update_if_password_is_present
     login_as :trevor
     put :update, :id => 4, :user => { :password => "ok", :password_confirmation => "" }
@@ -114,7 +114,7 @@ class UsersControllerTest < ActionController::TestCase
     users(:trevor).reload
     assert_equal User.encrypt('test'), users(:trevor).password_hash
   end
-  
+
   def test_should_not_change_password_if_left_blank_in_edit_form
     login_as :trevor
     put :update, :id => 4, :user => { :bio => 'it works!', :password => "", :password_confirmation => "" }
@@ -129,7 +129,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_path
     assert_equal num_users + 1, User.count
   end
-  
+
   def test_create_redirects_to_login_if_site_private_and_not_logged_in
     private_site
     num_users = User.count
@@ -137,7 +137,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_path
     assert_equal num_users, User.count
   end
-  
+
   def test_create_works_if_site_private_and_user_is_logged_in
     private_site
     login_as :trevor
@@ -150,37 +150,37 @@ class UsersControllerTest < ActionController::TestCase
   def test_should_not_create_user_without_login
     num_users = User.count
     post :create, :user => {:login => '', :email => 'test@test.com', :password => 'test'}
-    assert_template "new" 
+    assert_template "new"
     assert_equal num_users, User.count
   end
-  
+
   def test_should_not_create_user_without_email
     num_users = User.count
     post :create, :user => {:login => 'test', :email => '', :password => 'test'}
-    assert_template "new" 
+    assert_template "new"
     assert_equal num_users, User.count
   end
-  
+
   def test_should_not_create_user_without_valid_email
     num_users = User.count
     post :create, :user => {:login => 'test', :email => 'not_valid', :password => 'test'}
-    assert_template "new" 
+    assert_template "new"
     assert_equal num_users, User.count
   end
 
   def test_should_not_create_user_without_password_confirmation
     num_users = User.count
     post :create, :user => {:login => 'skdj', :email => 'test@test.com', :password => 'dfj', :password_confirmation => ''}
-    assert_template "new" 
+    assert_template "new"
     assert_equal num_users, User.count
   end
-  
+
   def test_bad_login_fails
     post :login, :user => {:login => 'skdj', :password => 'dfj'}
-    assert_template "login" 
+    assert_template "login"
     assert_equal "Invalid user/password combination", flash[:notice]
   end
-  
+
   def test_good_login_works
     post :login, :user => {:login => 'trevor', :password => 'test'}
     assert_equal 4, session[:user_id]
@@ -194,7 +194,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_not_nil users(:trevor).auth_token
     assert_not_nil users(:trevor).auth_token_exp
   end
-  
+
   def test_bad_login_does_not_set_auth_token
     post :login, :user => {:login => 'trevor', :password => 'bad'}
     users(:trevor).reload
@@ -202,23 +202,23 @@ class UsersControllerTest < ActionController::TestCase
     assert_nil users(:trevor).auth_token
     assert_nil users(:trevor).auth_token_exp
   end
-  
+
   def test_good_auth_token_login
-    @request.cookies["auth_token"] = CGI::Cookie.new("auth_token", "244cd62e5130681b86c01f8de9e9762d9a3f3645")    
+    @request.cookies["auth_token"] = CGI::Cookie.new("auth_token", "244cd62e5130681b86c01f8de9e9762d9a3f3645")
     assert @request.cookies["auth_token"]
     assert_not_nil users(:Timothy).auth_token_exp
     get :index
     assert_equal 3, session[:user_id]
   end
- 
+
   def test_expired_auth_token_login
-    @request.cookies["auth_token"] = CGI::Cookie.new("auth_token", "153c53039f6e8e8ca832d1512702f412298ec3a9")    
+    @request.cookies["auth_token"] = CGI::Cookie.new("auth_token", "153c53039f6e8e8ca832d1512702f412298ec3a9")
     assert @request.cookies["auth_token"]
     assert_not_nil users(:Administrator).auth_token_exp
     get :index
     assert_nil session[:user_id]
   end
-  
+
   def test_login_action_doesnt_bomb_with_bogus_params
     post :login
     assert_response :success
@@ -227,37 +227,37 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
     assert_nil session[:user_id]
   end
-  
+
   def test_should_not_error_if_trying_to_log_in_when_logged_in_already
     login_as :trevor
     post :login, :user => {:login => 'trevor', :password => 'test'}
     assert_redirected_to root_path
   end
-  
+
   def test_should_not_error_if_trying_to_logout_in_when_not_logged_in
     get :logout
     assert_redirected_to root_path
   end
-  
+
   # def test_current_user_stays_the_same_when_new_user_created
   # end
-  
+
   def test_should_not_allow_banned_user_to_login
     login_as :banned
     get :index
     assert_redirected_to logout_path
   end
-  
+
   def test_should_show_user_as_online_if_online_at_within_last_5_minutes
   end
-  
+
   def test_confirm_delete_page_works
     login_as :Administrator
     get :confirm_delete, :id => 1
     assert_response :success
     assert_template 'confirm_delete'
   end
-  
+
   def test_delete_user_works_if_self
     login_as :Timothy
     old_count = User.count
@@ -265,7 +265,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_equal old_count-1, User.count
     assert_redirected_to users_path
   end
-  
+
   def test_delete_user_works_if_admin
     login_as :Administrator
     old_count = User.count
@@ -273,7 +273,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_equal old_count-1, User.count
     assert_redirected_to users_path
   end
-  
+
   def test_delete_user_does_not_work_if_not_admin_or_self
     login_as :Timothy
     old_count = User.count
@@ -281,37 +281,37 @@ class UsersControllerTest < ActionController::TestCase
     assert_equal old_count, User.count
     assert_redirected_to root_path
   end
-  
+
   def test_delete_user_does_not_work_if_not_logged_in
     old_count = User.count
     delete :destroy, :id => 1, :confirm => 1
     assert_equal old_count, User.count
     assert_redirected_to root_path
   end
-    
+
   def test_that_default_time_zone_works
     post :create, :user => {:login => 'timezone', :email => 'test@test.com', :password => 'dfj', :password_confirmation => 'dfj'}
     user = User.find_by_login('timezone')
     assert_equal user.time_zone, 'US/Central'
   end
-  
+
   def test_that_user_gets_default_time_values
     post :create, :user => {:login => 'user1', :email => 'test1@test.com', :password => 'abc', :password_confirmation => 'abc'}
     assert_not_nil User.find_by_login('user1').online_at
     assert_not_nil User.find_by_login('user1').all_viewed_at
   end
-  
+
   def test_that_user_gets_default_time_zone_values
     post :create, :user => {:login => 'user1', :email => 'test1@test.com', :password => 'abc', :password_confirmation => 'abc'}
     assert_equal User.find_by_login('user1').time_zone, 'US/Central'
   end
-  
+
   def test_should_get_ban_if_admin
     login_as :Administrator
     get :ban, :id => users(:trevor).id
     assert_response :success
   end
-  
+
   def test_should_not_get_ban_if_not_admin_or_not_logged_in
     get :ban, :id => users(:trevor).id
     assert_redirected_to root_path
@@ -319,7 +319,16 @@ class UsersControllerTest < ActionController::TestCase
     get :ban, :id => users(:trevor).id
     assert_redirected_to root_path
   end
-  
+
+  test "should ban user if admin" do
+    login_as :Administrator
+    ban_time = 1.year.from_now
+    put :update, :id => users(:trevor).id, :user => {:ban_message => 'get banned!', :banned_until => ban_time}
+    users(:trevor).reload
+    assert_equal users(:trevor).ban_message, 'get banned!'
+    assert users(:trevor).banned?
+  end
+
   def test_should_remove_ban_if_admin
     login_as :Administrator
     post :remove_ban, :id => users(:banned).id
@@ -328,7 +337,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_nil users(:banned).ban_message
     assert_nil users(:banned).banned_until
   end
-  
+
   def test_should_not_remove_ban_if_not_admin_or_not_logged_in
     post :remove_ban, :id => users(:banned).id
     assert_redirected_to root_path
@@ -336,7 +345,7 @@ class UsersControllerTest < ActionController::TestCase
     post :remove_ban, :id => users(:banned).id
     assert_redirected_to root_path
   end
-  
+
   def test_should_grant_admin_if_admin
     login_as :Administrator
     post :admin, :id => users(:trevor).id
@@ -344,7 +353,7 @@ class UsersControllerTest < ActionController::TestCase
     users(:trevor).reload
     assert_equal users(:trevor).admin, true
   end
-  
+
   def test_should_revoke_admin_if_admin
     login_as :Administrator
     post :admin, :id => users(:Administrator).id
@@ -352,7 +361,7 @@ class UsersControllerTest < ActionController::TestCase
     users(:Administrator).reload
     assert_equal users(:Administrator).admin, false
   end
-  
+
   def test_should_not_toggle_admin_if_not_admin_or_not_logged_in
     post :admin, :id => users(:trevor).id
     users(:trevor).reload
@@ -370,7 +379,7 @@ class UsersControllerTest < ActionController::TestCase
     users(:trevor).reload
     assert_equal false, users(:trevor).logged_out
   end
-  
+
   def test_should_set_logged_out_to_true_when_logging_out
     login_as :trevor
     get :logout
