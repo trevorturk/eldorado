14441ae8c2e7a59e30cbc32537443db4d47a71eb
Trevor Turk <trevorturk@gmail.com>
2010-02-09 21:12:43 -0600

    tweaks to disable tabs feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,21 @@
-module ApplicationHelper  
-  
+module ApplicationHelper
+
   def random_string
     char = ("a".."z").to_a + ("1".."9").to_a
     Array.new(6, '').collect{char[rand(char.size)]}.join
   end
-    
+
   protected
-  
+
   def header_image_path
     if current_controller == 'headers' && %w(edit show).include?(current_action)
       @header = Header.find(params[:id])
     else
       @header = Header.random
     end
-
     @header and @header.attachment.url or "/images/eldorado.jpg"
   end
-  
+
   def header_css
     if @settings.clickable_header
       return ""
@@ -24,10 +23,10 @@ module ApplicationHelper
       return "<style type=\"text/css\">.header { background: url('#{header_image_path}'); }</style>"
     end
   end
-    
+
   def theme_css
-    return "<style type=\"text/css\">@import url('/stylesheets/application.css');</style>" if @settings.theme.blank? 
-    return "<style type=\"text/css\">@import url('#{@settings.theme}');</style>"    
+    return "<style type=\"text/css\">@import url('/stylesheets/application.css');</style>" if @settings.theme.blank?
+    return "<style type=\"text/css\">@import url('#{@settings.theme}');</style>"
   end
 
   def page_title
@@ -36,15 +35,15 @@ module ApplicationHelper
     page = @settings.tagline if current_controller == 'home'
     "#{@settings.title}: #{item || page}"
   end
-  
+
   def favicon_tag
     return "<link rel=\"shortcut icon\" href=\"#{@settings.favicon}\" />\n" unless @settings.favicon.blank?
   end
-  
+
   def avatar_for(user)
     image_tag user.avatar unless user.avatar.nil?
   end
-  
+
   def rank_for(user)
     return I18n.t(:rank_administrator) if user.admin
     return I18n.t(:rank_banned) if user.banned?
@@ -56,7 +55,7 @@ module ApplicationHelper
     return I18n.t(:rank_member) if @rank.nil? # if no ranks are low enough
     return h(@rank.title)
   end
-  
+
   def tab(name)
     if name == current_controller
       'current_tab'
@@ -66,7 +65,12 @@ module ApplicationHelper
       'current_tab'
     end
   end
-  
+
+  def enabled?(name)
+    return true if CONFIG['disabled_tabs'].nil?
+    !CONFIG['disabled_tabs'].include?(name)
+  end
+
   def is_new?(item)
     return false unless logged_in?
     if item.is_a?(Topic)
@@ -76,7 +80,7 @@ module ApplicationHelper
       session[:online_at] < item.updated_at
     end
   end
-  
+
   def icon_for(item)
     if item && is_new?(item)
       '<div class="icon inew"><!-- --></div>'
@@ -84,11 +88,11 @@ module ApplicationHelper
       '<div class="icon"><!-- --></div>'
     end
   end
-  
+
   def bb(text)
     auto_link(simple_format(bbcodeize(sanitize(h(text))))) {|t| truncate(t, :length => 50)}
   end
-  
+
   def current_page(collection)
     I18n.t(:page)+' ' + number_with_delimiter(collection.current_page).to_s + ' ' + I18n.t(:of) + ' ' + number_with_delimiter(collection.total_pages).to_s unless collection.total_pages == 0
   end
@@ -98,18 +102,17 @@ module ApplicationHelper
       link_to('&laquo; '+I18n.t(:previous_page), { :page => collection.previous_page }.merge(params.reject{|k,v| k=='page'}))
     end
   end
-  
+
   def next_page(collection)
     unless collection.current_page == collection.total_pages or collection.total_pages == 0
       link_to(I18n.t(:next_page)+' &raquo;', { :page => collection.next_page }.merge(params.reject{|k,v| k=='page'}))
     end
   end
-  
+
   def t_no_of(item, count = 0)
     I18n.t item, :count => count, :print_count => number_with_delimiter(count)
   end
 
-  
   def time_ago_or_time_stamp(from_time, to_time = Time.now.utc, include_seconds = true, detail = false)
     return '&ndash;' if from_time.nil?
     from_time = from_time.to_time if from_time.respond_to?(:to_time)
@@ -128,17 +131,8 @@ module ApplicationHelper
     return time_stamp(from_time) if (detail && distance_in_minutes > 2880)
     return time
   end
-  
+
   def time_stamp(time, short = false)
-    I18n.l(time, :format => ( short ? :ed_timestamp_short : :ed_timestamp ) ) 
+    I18n.l(time, :format => ( short ? :ed_timestamp_short : :ed_timestamp ) )
   end
-
-  def disabled_feature?(f)
-    # Admins can see all features
-    return false if (current_user != 0 and current_user.admin)
-
-    disabled_features = (CONFIG["disabled_features"] or "").split(",").map { |x| x.strip }
-    disabled_features.include?(f)
-  end
-  
 end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,41 +1,51 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  
+
   def test_should_get_index
     get :index
     assert_response :success
   end
-  
+
   def test_should_get_index_when_logged_in
     login_as :trevor
     get :index
     assert_response :success
   end
-  
+
   def test_should_get_index_when_not_logged_in
     get :index
     assert_response :success
   end
-  
+
   def test_should_redirect_to_login_if_site_is_private_and_not_logged_in
     private_site
     get :index
     assert_redirected_to login_path
   end
-  
+
   def test_should_get_index_if_site_is_private_and_logged_in
     login_as :trevor
     private_site
     get :index
     assert_response :success
   end
-  
+
   def test_should_get_help
     get :help
     assert_response :success
   end
-  
+
+  test "should allow disbaled tabs" do
+    get :index
+    assert_response :success
+    assert_select '#tab-blog'
+    silence_warnings {CONFIG['disabled_tabs'] = 'blog events'}
+    get :index
+    assert_response :success
+    assert_select '#tab-blog', false
+  end
+
   # def test_should_not_update_session_online_at
   #   # if current_user HAS been active in the last 10 minutes
   # end
