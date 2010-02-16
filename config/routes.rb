d8701220a4076f5c224da8d0216a9e397bca57f3
Trevor Turk <trevorturk@gmail.com>
2010-02-16 08:12:38 -0600

    remove old logged_exceptions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  
   map.root :controller => 'home'
-  
+
   map.resources :articles
   map.resources :avatars, :member => {:select => :post, :deselect => :post}
   map.resources :categories, :member => {:confirm_delete => :get}
@@ -17,16 +16,15 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :themes, :member => {:select => :post, :deselect => :post}
   map.resources :topics, :member => {:show_new => :get}, :collection => {:mark_all_viewed => :get}
   map.resources :uploads
-  map.resources :users, :member => {:admin => :post, :ban => :get, :remove_ban => :post, :confirm_delete => :get}, 
-                        :has_many => [:articles, :posts]
-  
+  map.resources :users, :member => {:admin => :post, :ban => :get, :remove_ban => :post, :confirm_delete => :get}, :has_many => [:articles, :posts]
+
   map.search 'search', :controller => 'search', :action => 'index'
   map.refresh_chatters 'refresh_chatters', :controller => 'messages', :action => 'refresh_chatters'
-  
+
   map.login 'login', :controller => 'users', :action => 'login'
   map.logout 'logout', :controller => 'users', :action => 'logout'
   map.register 'register', :controller => 'users', :action => 'new'
-  
+
   map.admin 'admin', :controller => 'settings', :action => 'index'
   map.blog 'blog', :controller => 'articles', :action => 'index'
   map.blog_archives 'blog/archives', :controller => 'articles', :action => 'archives'
@@ -34,9 +32,6 @@ ActionController::Routing::Routes.draw do |map|
   map.files 'files', :controller => 'uploads', :action => 'index'
   map.forum_root 'forum', :controller => 'forums', :action => 'index'
   map.help 'help', :controller => 'home', :action => 'help'
-  
-  map.exceptions 'logged_exceptions/:action/:id', :controller => 'logged_exceptions', :action => 'index', :id => nil
-  
+
   map.catch_all '*path', :controller => 'topics', :action => 'unknown_request'
-  
 end
